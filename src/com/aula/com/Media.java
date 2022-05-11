7aa217f9c62ad8525709c4e5e438039524290a56
delvan <delvanpj@gmail.com>
2022-05-10 21:55:07 -0300

    Adicionando situação do aluno

--- src/com/aula/com/Media.java
+++ src/com/aula/com/Media.java
@@ -16,19 +16,31 @@ public class Media {
 		nomeAluno = teclado.next();
 		
 		System.out.println("Digite a nota 1: ");
-		nota1 = teclado.nextInt();
+		nota1 = teclado.nextDouble();
 		
 		System.out.println("Digite a nota 2: ");
-		nota2 = teclado.nextInt();
+		nota2 = teclado.nextDouble();
 				
 		System.out.println("Digite a nota 3: ");
-		nota3 = teclado.nextInt();
+		nota3 = teclado.nextDouble();
 		
 		System.out.println("Digite a nota 4: ");
-		nota4 = teclado.nextInt();
+		nota4 = teclado.nextDouble();
+		
+		
 		
 		
 		media = (nota1 + nota2 + nota3 + nota4)/ 4;
+		
+		if(media <5) {
+			System.out.println("Aluno Reprovado ");
+		}else if(media == 5){
+			System.out.println("Aluno Recuperação");
+		}
+		else {
+			System.out.println("Situação do aluno");
+			System.out.println("Aluno Aprovado \n");
+		}
 		
 		System.out.println("Resultado");
 		System.out.println("Nota 1 "+ nota1);
